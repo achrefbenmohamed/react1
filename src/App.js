1d5b4a4235ca9e072ecfa6517aeab0f17720d7a2
achrefbenmohamed <achref.bmohameed@gmail.com>
2022-10-24 14:46:19 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+
+import './App.css';
+import { Navbar,Container,Nav,Form} from "react-bootstrap"
+function App() {
+  return (
+    
+    <div>
+       <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Form.Group className="mb-3">
+        <Form.Label>Disabled input</Form.Label>
+        <Form.Control placeholder="Disabled input" disabled />
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Disabled select menu</Form.Label>
+        <Form.Select disabled>
+          <option>Disabled select</option>
+        </Form.Select>
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Check type="checkbox" label="Can't check this" disabled />
+      </Form.Group>
+      
+    </div>
+  );
+}
+
+export default App;
